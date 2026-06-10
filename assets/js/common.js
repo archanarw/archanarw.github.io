afdd33edcf92e6a670964da7dc40e3d156f8bb3f
archanarw <47085068+archanarw@users.noreply.github.com>
2026-06-10 18:02:17 -0400

    update fonts + styling

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,19 +1,28 @@
 $(document).ready(function () {
-  // add toggle functionality to abstract, award and bibtex buttons
+  // add toggle functionality to abstract, award, bibtex, and poster buttons
   $("a.abstract").click(function () {
     $(this).parent().parent().find(".abstract.hidden").toggleClass("open");
     $(this).parent().parent().find(".award.hidden.open").toggleClass("open");
     $(this).parent().parent().find(".bibtex.hidden.open").toggleClass("open");
+    $(this).parent().parent().find(".poster.hidden.open").toggleClass("open");
   });
   $("a.award").click(function () {
     $(this).parent().parent().find(".abstract.hidden.open").toggleClass("open");
     $(this).parent().parent().find(".award.hidden").toggleClass("open");
     $(this).parent().parent().find(".bibtex.hidden.open").toggleClass("open");
+    $(this).parent().parent().find(".poster.hidden.open").toggleClass("open");
   });
   $("a.bibtex").click(function () {
     $(this).parent().parent().find(".abstract.hidden.open").toggleClass("open");
     $(this).parent().parent().find(".award.hidden.open").toggleClass("open");
     $(this).parent().parent().find(".bibtex.hidden").toggleClass("open");
+    $(this).parent().parent().find(".poster.hidden.open").toggleClass("open");
+  });
+  $("a.poster").click(function () {
+    $(this).parent().parent().find(".abstract.hidden.open").toggleClass("open");
+    $(this).parent().parent().find(".award.hidden.open").toggleClass("open");
+    $(this).parent().parent().find(".bibtex.hidden.open").toggleClass("open");
+    $(this).parent().parent().find(".poster.hidden").toggleClass("open");
   });
   $("a").removeClass("waves-effect waves-light");
 
